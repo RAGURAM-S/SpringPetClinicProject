7a531ae2ba0b1ef420415a8da152a0c360283c1e
RAGURAM-S <raguramsathasivam@gmail.com>
2020-03-22 19:50:26 +0530

    Initial Project Commit

---
+++ PetClinicProject/src/test/java/com/springFrameWork/springPetClinic/PetClinicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springFrameWork.springPetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
